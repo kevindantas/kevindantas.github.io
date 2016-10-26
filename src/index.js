c0eee8259ddbd35503888b49e37d680b3f3afe94
Kevin-ds <kevin.ds.29@gmail.com>
2016-10-25 22:03:37 -0200

    Add react-router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import Layout from './Layout';
+import Home from './components/Home'
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+  	<Route path="/" component={Layout}>
+  		<IndexRoute component={Home} />
+  	</Route>
+  </Router>,
   document.getElementById('root')
 );
