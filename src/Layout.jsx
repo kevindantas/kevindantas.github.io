0d04af0af3fbaabc37aedb9fc2b96ac2eb6ecd45
Kevin-ds <kevin.ds.29@gmail.com>
2016-11-01 23:37:25 -0200

    NavBar shows on scroll

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
-import logo from './logo.svg';
-import logoKevin from './images/LogoKevin.svg';
 import './Layout.css';
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div className="Layout">
-        <header className="Layout-header">
-          <nav>
-            <Link href="/">
-              <img src={logoKevin} alt="Kevin Dantas logo" className="Layout-logo" />
-            </Link>
-
-            <ul className="Layout-menu">
-              <li>
-                <Link href="#work">Work</Link>
-              </li>
-              <li>
-                <Link href="#contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <NavBar />
 
         { this.props.children }
 
--- src/components/landing/Banner.jsx
+++ src/components/landing/Banner.jsx
@@ -4,12 +4,21 @@ import './Banner.css'
 
 
 export default class Banner extends Component {
+
+	componentDidMount() {
+		var lines = this.refs.canvasLines;
+		lines.width = window.innerWidth;
+		lines.height = window.innerHeight;
+	}
+
 	render() {
 		return (
 			<section className="Banner">
 				<div className="Banner-logo-wrapper">
 					<img src={logoKevin} alt="Kevin Dantas" />
 				</div>
+
+				<canvas ref="canvasLines" id="CanvasLines"></canvas>
 			</section>
 		);
 	}
