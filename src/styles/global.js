c766fb48008710d59753e4d440c3c636f644d886
Kevin-ds <kevin.ds.29@gmail.com>
2017-12-31 03:23:26 -0200

    Add global styles

---
+++ src/styles/global.js
@@ -0,0 +1,15 @@
+import { injectGlobal } from 'emotion';
+
+injectGlobal`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background: #002745;
+    color: #fff;
+    font-family: 'Helvetica', sans-serif;
+  }
+`;
