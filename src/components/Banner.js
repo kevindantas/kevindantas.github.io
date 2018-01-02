0f797ac2367c1823f5e01660361d80d69d4322e4
Kevin-ds <kevin.ds.29@gmail.com>
2018-01-02 17:22:15 -0200

    Create scroll down indicator

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,11 @@
 import { h, Component } from 'preact';
+import { css } from 'emotion';
 import styled from 'preact-emotion';
 import VanillaTilt from 'vanilla-tilt';
 import Logo from './Logo';
 import lines from '../images/lines.svg';
 import { fullScreenCenter, flexCenter } from '../styles/helpers';
+import { arrowPulse } from '../styles/animations';
 
 const lineColor = 'rgba(255, 255, 255, 0.1)';
 const patternSize = '20px';
@@ -52,6 +54,42 @@ const Circle = styled.div`
   }
 `;
 
+
+const downArrow = css`
+  content: '';
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  left: calc(50% - 15px);
+  bottom: 0;  
+  transform: rotate(45deg);
+  border-bottom: 1.5px solid white;
+  border-right: 1.5px solid white;
+  animation: ${arrowPulse} 3s 1s ease-in-out infinite;
+  opacity: 0.5;
+`;
+
+const ScrollMessage = styled.h3`
+  font-weight: lighter;
+  position: absolute;
+  bottom: 3vh;
+  text-transform: uppercase;
+  padding-bottom: 60px;
+
+  &::before {
+    ${downArrow};
+    bottom: 25px;
+  }
+  
+  &::after {
+    ${downArrow};
+    width: 50px;
+    height: 50px;
+    left: calc(50% - 25px);
+    animation-delay: 1.2s;
+  }
+`;
+
 class Banner extends Component {
   componentDidMount() {
     VanillaTilt.init(this.circle.base, {
@@ -73,6 +111,8 @@ class Banner extends Component {
         >
           <Logo width={200} height={200} style={{ transform: 'translateZ(30px)' }} />
         </Circle>
+
+        <ScrollMessage>Scroll down</ScrollMessage>
       </Wrapper>
     );
   }
---
+++ src/styles/animations.js
@@ -0,0 +1,21 @@
+import { keyframes } from 'emotion';
+
+export const arrowPulse = keyframes`
+  0% {
+    transform: scale(1) rotate(45deg);
+  }
+
+  15% {
+  transform: scale(1) rotate(45deg);
+  }
+
+  20% {
+  transform: scale(1.2) rotate(45deg);
+  }
+
+  25% {
+    transform: scale(1) rotate(45deg);
+  }
+`;
+
+export const shake = keyframes``;
