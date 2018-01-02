75b680d4e52d254628813e1642194cc8366900b9
Kevin-ds <kevin.ds.29@gmail.com>
2018-01-02 00:22:13 -0200

    Create a parallax effect

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,20 +2,54 @@ import { h, Component } from 'preact';
 import styled from 'preact-emotion';
 import VanillaTilt from 'vanilla-tilt';
 import Logo from './Logo';
+import lines from '../images/lines.svg';
 import { fullScreenCenter, flexCenter } from '../styles/helpers';
+
+const lineColor = 'rgba(255, 255, 255, 0.1)';
+const patternSize = '20px';
 
 const Wrapper = styled.section`
   ${fullScreenCenter};
+  background: linear-gradient(45deg, transparent, transparent 49%, ${lineColor} 50%, transparent 51%, transparent),
+              linear-gradient(-45deg, transparent, transparent 49%, ${lineColor} 50%, transparent 51%, transparent);
+  background-size: ${patternSize} ${patternSize};
 `;
 
 const Circle = styled.div`
-  width: 400px;
-  height: 400px;
+  background: #002745;
+  width: 320px;
+  height: 320px;
   border-radius: 1000px;
+  position: relative;
   text-align: center;
-  box-shadow: 1px 5px 25px 5px rgba(0, 0, 0, 0.15);
+  box-shadow: 1px 5px 25px rgba(0, 0, 0, 0.15);
   transform-style: preserve-3d;
   ${flexCenter};
+
+  &:hover {
+    box-shadow: 1px 5px 40px 2px rgba(0, 0, 0, 0.25);
+  }
+
+  &::before {
+    content: '';
+    width: inherit;
+    height: inherit;
+    border-radius: inherit;
+    position: absolute;
+    border: 2px dashed rgba(255,255,255, 0.4);
+    transform: translateZ(-25px) scale(1.2);
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: inherit;
+    height: inherit;
+    border-radius: inherit;
+    background: url(${lines}) center no-repeat;
+    background-size: 70%;
+    transform: translateZ(-7px) scale(1.9);
+  }
 `;
 
 class Banner extends Component {
